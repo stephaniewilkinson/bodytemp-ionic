7816fcae8d5365fe873371b24f5a7224277ca8ea
Stephanie Wilkinson <what.happens@gmail.com>
2016-07-19 11:52:25 -0700

    adds logs dummy data

--- www/js/services.js
+++ www/js/services.js
@@ -47,4 +47,53 @@ angular.module('starter.services', [])
       return null;
     }
   };
+})
+
+.factory('Logs', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var logs = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return logs;
+    },
+    remove: function(log) {
+      logs.splice(logs.indexOf(log), 1);
+    },
+    get: function(logId) {
+      for (var i = 0; i < logs.length; i++) {
+        if (logs[i].id === parseInt(logId)) {
+          return logs[i];
+        }
+      }
+      return null;
+    }
+  };
 });
