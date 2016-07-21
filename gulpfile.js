833da627de324ef0f46a2717bb739e47afb25aca
Stephanie Wilkinson <what.happens@gmail.com>
2016-07-21 13:48:21 -0700

    adds ionic-materialize

--- gulpfile.js
+++ gulpfile.js
@@ -49,3 +49,81 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
+gulp.task('js', function () {
+    var src = [
+        './src/js/lib/_motion.js',
+        './src/js/lib/_ink.js'
+    ];
+    var js = gulp.src(src)
+        .pipe(concat('ionic.material.js'))
+        .pipe(gulp.dest(distPath));
+
+    if (minify) {
+        js.pipe(uglify())
+            .pipe(rename('ionic.material.min.js'))
+            .pipe(gulp.dest(distPath))
+    }
+    return js;
+});
+
+gulp.task('serve', function(){
+    return connect.server();
+});
+
+
+gulp.task('webpack', function(){
+    var webpackConfig = require('./webpack.config.js');
+    var uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
+        sourcemaps: false
+    });
+
+    var minifiedConfig = _.cloneDeep(webpackConfig);
+    if(!minifiedConfig.plugins || _.isEmpty(minifiedConfig.plugins)){
+        minifiedConfig.plugins = [];
+    }
+    minifiedConfig.plugins.push(uglifyPlugin);
+    minifiedConfig.output.filename = 'ionic.material.min.js';
+
+    return gulp.src('src/ionic-material.js')
+      .pipe(gulpWebpack(webpackConfig))
+      .pipe(gulp.dest(distPath))
+      .pipe(rename('ionic.material.js'))
+      .pipe(gulp.src('src/ionic-material.js')) // dunno if this is needed, just getting unminified src again
+      .pipe(gulpWebpack(minifiedConfig))
+      .pipe(gulp.dest(distPath))
+      .pipe(rename('ionic.material.min.js'));
+});
+
+gulp.task('styles', function () {
+    var src = './src/scss/index.scss';
+
+    var scss = gulp.src(src)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(rename('ionic.material.css'));
+
+
+    if (minify) {
+        scss.pipe(gulp.dest(distPath)).pipe(rename({
+            suffix: '.min'
+        }))
+            .pipe(minifycss())
+            .pipe(rename('ionic.material.min.css'))
+            .pipe(sourcemaps.write()).pipe(gulp.dest(distPath));
+    } else {
+        scss.pipe(sourcemaps.write()).pipe(gulp.dest(distPath));
+    }
+
+    return scss;
+});
+
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch(['./*.js', '!./src/js/'], ['webpack']);
+    gulp.watch('./*.scss', ['styles']);
+});
+
+gulp.task('build', function () {
+    minify = true;
+    return gulp.start(['webpack', 'styles']);
+})
--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'chart.js', 'starter.controllers', 'starter.services', 'ui.rCalendar', 'firebase', 'ngAnimate'])
+angular.module('starter', ['ionic', 'chart.js', 'starter.controllers', 'starter.services', 'ui.rCalendar', 'firebase', 'ngAnimate', 'ionic-material'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -96,6 +96,6 @@ angular.module('starter', ['ionic', 'chart.js', 'starter.controllers', 'starter.
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/signup');
 
 });
